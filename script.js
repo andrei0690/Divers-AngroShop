882b761a217e838ae08f37045a7bb6033aceae06
andrei0690 <andreiarsenii90@gmail.com>
2022-12-25 21:04:29 +0200

    added basic functions to main.html

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+let elements = document.getElementsByTagName("li");
+console.log(elements[0].innerHTML)
+
 let theme = document.querySelector(".theme");
 theme.classList.add("remover");
-document.title = "hello"
+document.title = "Divers AgroShop";
 function active(){
 	theme.classList.remove("remover");
 }
 function delete_menu(){
 	theme.classList.add("remover");
 }
-console.log("hhehhe");
+
+function light_theme(){
+
+	elements[0].innerHTML = `Tema Intunecata <i class="fa-solid fa-moon"></i>`;
+	document.body.style.backgroundColor = "black";
+
+}
+
+function dark_theme(){
+	elements[0].innerHTML = `Tema Luminoasa <i class="fa-solid fa-sun"></i>`;
+	document.body.style.backgroundColor = "black";
+}
+
+
