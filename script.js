fd21fd25c54b8de58dde296afd2a499e5c52bd28
andrei0690 <andreiarsenii90@gmail.com>
2022-12-25 12:55:48 +0200

    Added menu and product boxes

---
+++ script.js
@@ -0,0 +1,10 @@
+let theme = document.querySelector(".theme");
+theme.classList.add("remover");
+document.title = "hello"
+function active(){
+	theme.classList.remove("remover");
+}
+function delete_menu(){
+	theme.classList.add("remover");
+}
+console.log("hhehhe");
