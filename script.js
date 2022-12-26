59250a2699d1c24b1625285caac8d1c74f05c820
andrei0690 <andreiarsenii90@gmail.com>
2022-12-26 17:49:20 +0200

    made main page

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let elements = document.getElementsByTagName("li");
-console.log(elements[0].innerHTML)
-
+let promotion = document.querySelector(".promotion");
+let container = document.querySelector(".container");
+let change = true;
 let theme = document.querySelector(".theme");
 theme.classList.add("remover");
 document.title = "Divers AgroShop";
@@ -11,16 +12,26 @@ function delete_menu(){
 	theme.classList.add("remover");
 }
 
-function light_theme(){
+function change_theme(){
 
-	elements[0].innerHTML = `Tema Intunecata <i class="fa-solid fa-moon"></i>`;
-	document.body.style.backgroundColor = "black";
-
+	if (change == false){
+		elements[0].innerHTML = `Tema Intunecata <i class="fa-solid fa-moon"></i>`;
+		document.body.style.backgroundColor = "white";
+		change = true;
+		return 0;
+	}
+	else if(change = true)
+	{
+		elements[0].innerHTML = `Tema Luminoasa <i class="fa-solid fa-sun"></i>`;
+		document.body.style.backgroundColor = "#2F2F2F";
+		change = false;
+		return 0;
+	}
 }
 
-function dark_theme(){
-	elements[0].innerHTML = `Tema Luminoasa <i class="fa-solid fa-sun"></i>`;
-	document.body.style.backgroundColor = "black";
-}
+function destroy_promotion(){
 
+	promotion.remove();
+	container.classList.add("unblur");
+}
 
