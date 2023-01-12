454d129e00060d31473575b4b1ead4656219a5bd
Arsitur <arsitur4@gmail.com>
2023-01-13 00:02:07 +0200

    Fix la bug cu descrierea si optimizat pentru general

--- contact_us.js
+++ contact_us.js
@@ -1,23 +1,4 @@
-const observer = new IntersectionObserver((entries) =>
-{
-	entries.forEach((entry) =>
-	{
-		console.log(entry);
-		if(entry.isIntersecting)
-		{
-			entry.target.classList.add("show");	
-		}
-		
-	})
-})
-
-const hiddenElements = document.querySelectorAll('.hidden');
-
-hiddenElements.forEach((el) =>
-{
-	observer.observe(el);
-})
-
+// Rezolva Bug-ul unde descrierea are deja spatiu in ea
 const contactDescription = document.querySelector('.description-input');
 
 contactDescription.value = '';
