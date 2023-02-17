4d83f9fefab1457e5e638254010e622ca68db159
Artemis <arsitur4@gmail.com>
2023-02-17 22:36:08 +0200

    Rezolvat problema cu about_us, modificat backup-button, adaugat mai mult scris la orar, etc

--- about_us.js
+++ about_us.js
@@ -22,126 +22,6 @@ if(window.innerWidth <= 750 && staffSwitched == false)
 updateStaffSection();
 
 
-// BackUp Button
-
-// Calculam procentul la pagina ca sa apara butonul mereu dupa o anumita josime
-
-const backupButtons = document.querySelectorAll('.backup-button');
-const backUpButton = document.querySelector('.backup-div');
-
-window.addEventListener('scroll', () => {
-    
-    const {scrollTop, scrollHeight} = document.documentElement;
-
-    const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100;
-
-
-
-  	if (scrollPercent >= 23) 
-  	{
-  		if(globalbackupbuttons == 1)
-  		{
-  			backUpButton.classList.add('display-backup-button-div');
-  		}
-	 }
-	 else
-	 {
-		backUpButton.classList.remove('display-backup-button-div');
-	 }
-})
-
-// Cand apesi te duce sus 
-backupButtons.forEach(button =>
-{
-	button.addEventListener('click', () => {
-	  window.scrollTo({
-	    top: 0,
-	    behavior: 'smooth'
-	  });
-	});
-})
-
-// Cand vede ultima sectiune sa dispara
-const diversOrarDiv = document.querySelector('.divers-orar-div');
-const createdBackupButtonDivHTML = document.querySelector('.created-backup-button-div-html');
-const createdBackupDiv = document.querySelector('#created-backup-div');
-const createdBackupButton = document.querySelector('#created-backup-button');
-const OrarSection = document.querySelector('.orar-info-section');
-const endSection =  document.querySelector('.end-section');
-const endSectionStart = document.querySelector('.end-section-start')
-globalbackupbuttons = 1;
-buttonMoved = false;
-
-function setBackupButton()
-{
-      let orarSectionRect = endSectionStart.offsetTop;
-      
-      backUpButton.classList.add('positioned-backup-button');
-      backupbuttontopVar = orarSectionRect + 'px';
-
-      backUpButton.classList.add('postion-backup-button-div');
-      backUpButton.style.setProperty('--top', backupbuttontopVar);
-
-      buttonMoved = true;
-}
-
-function unsetBackupButton()
-{
-      backUpButton.classList.remove('positioned-backup-button');
-      backUpButton.classList.remove('postion-backup-button-div');
-
-      buttonMoved = false;
-}
-
-
-
-const backUpbuttonObserver = new IntersectionObserver((entries) =>
-{
-	entries.forEach((entry) =>
-	{
-
-    const {scrollTop, scrollHeight} = document.documentElement;
-
-    const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100;
-    if(entry.isIntersecting && buttonMoved == false)
-    {
-      setBackupButton();
-    }
-    if(!entry.isIntersecting && buttonMoved == true && scrollPercent <= 85)
-    {
-      unsetBackupButton();
-    }
-  
-  })
-}
-)
-
-
-backUpbuttonObserver.observe(endSectionStart);
-
-function dissapearBackupButton()
-{
-  backUpButton.classList.add('dissapear-backup-button-div');
-  backUpButton.classList.remove('display-backup-button-div');
-
-}
-
-function createBackupButton()
-{
-   let realCreatedBackupButtonDiv = document.createElement('div');
-   realCreatedBackupButtonDiv.classList.add('display-backup-button-div');
-   realCreatedBackupButtonDiv.style.marginTop = '25px';
-
-   let realCreatedBackupButton = document.createElement('button');
-   realCreatedBackupButton.classList.add('backup-button');
-   realCreatedBackupButton.innerHTML = '<i class="fa-solid fa-arrow-up"></i>';
-
-   realCreatedBackupButtonDiv.appendChild(realCreatedBackupButton);
-
-   createdBackupButtonDivHTML.appendChild(realCreatedBackupButtonDiv);
-   
-}
-
 // Progress Bar
 
 const progressBar = document.querySelector('.progress-bar');
@@ -580,19 +460,11 @@ function addOwnerbars() {
     e.classList.remove('owner--dissapear');
   })
 }
-let setbuttontimeout;
+
 window.addEventListener("resize", () =>
 {
   //Update la staff carusel
   updateStaffSection();
-  //Backup Button elimina buguri
-  clearTimeout(setbuttontimeout);
-  unsetBackupButton();
-  scrollPercent = getScrollPercent();
-  if(scrollPercent >= 95 )
-  {
-    setbuttontimeout = setTimeout(setBackupButton, 500);
-  }
   
   //Piramida la staff
   if(window.innerWidth >= 1350)
